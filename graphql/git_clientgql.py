82d56c7e1c9a6b4bb97798ad0951b563631085d6
Roberto Palloni <rpdev20@gmail.com>
2021-11-04 18:11:14 +0100

    commit data

--- graphql/git_clientgql.py
+++ graphql/git_clientgql.py
@@ -1,6 +1,8 @@
 # GQL is for consuming GraphQL APIs
 # Graphene is for building GraphQL APIs
 
+import pytz # timezone
+import pandas as pd
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 #from gql.transport.aiohttp import AIOHTTPTransport # async request
@@ -10,7 +12,7 @@ headers = {'Authorization': 'token ' + token} # set token
 
 client = Client(transport=RequestsHTTPTransport(url='https://api.github.com/graphql', headers=headers))
 
-gql_query = gql(
+gql_query_repos = gql(
 """
 {
   viewer {
@@ -40,9 +42,61 @@ gql_query = gql(
 """
 )
 
-repo_data = client.execute(gql_query)['viewer']['repositories']['edges']
+repo_data = client.execute(gql_query_repos)['viewer']['repositories']['edges']
 
 repo_ncommit = {}
 for repo in repo_data:
   repo_ncommit[repo['node']['name']] = repo['node']['refs']['edges'][0]['node']['target']['history']['totalCount']
 print(repo_ncommit)
+
+
+gql_query_commits = gql(
+    """
+{
+  viewer {
+    repositories(first: 30) {
+      edges {
+        node {
+          name
+          refs(first: 100, refPrefix: "refs/heads/") {
+            edges {
+              node {
+                name
+                target {
+                  ... on Commit {
+                    history {
+                      edges {
+                        node {
+                          id
+                          committedDate
+                        }
+                      }
+                      totalCount
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+)
+
+commit_data = client.execute(gql_query_commits)['viewer']['repositories']['edges']
+
+df = pd.DataFrame(pd.json_normalize(commit_data, ['node', 'refs', 'edges', 'node', 'target', 'history', 'edges']))
+
+df['committedDateCET'] = pd.to_datetime(df['node.committedDate']).dt.tz_convert('Europe/Rome')
+df['committedYear'] = pd.to_datetime(df['committedDateCET']).dt.year
+df['committedDateCET'] = pd.to_datetime(df['committedDateCET']).dt.hour
+
+
+df.groupby(df["committedDateCET"]).count()
+
+df[df['committedYear'] == 2021].groupby(df["committedDateCET"]).count()
+
+df.to_csv('data.csv')
