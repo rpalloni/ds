159d5f2e152196fae3e2fca84bcd6d7ef8a43765
Roberto Palloni <rpdev20@gmail.com>
2021-11-12 18:19:01 +0100

    update NN

--- tensorflow/tf_keras.py
+++ tensorflow/tf_keras.py
@@ -4,12 +4,14 @@ models apis: Sequential, Functional, Subclassing
 '''
 
 import math
+import numpy as np
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense # standard neural network
 from keras.layers import LSTM # long-short term memory layer
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('air_pollution.csv', sep=' ')
 df.head()
@@ -18,3 +20,19 @@ df['DateTime'] = pd.to_datetime(df['Date'] + ' ' + df['Time']).dt.floor('h')
 df.drop(['Date', 'Time'], axis=1, inplace=True)
 
 df.groupby('DateTime').mean()
+
+df.describe()
+
+target = df['TEMP']
+predictors = df.drop('TEMP', axis=1)
+
+# LSTM Neural Network
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, 1)))
+model.add(Dense(1))
+
+# split data into training and test datasets
+trainX, testX, trainY, testY = train_test_split(predictors, target, test_size=0.25, random_state=123)
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=500, batch_size=32, verbose=2)
