c11425d6a9e57bc2f1dddd369d520e2b736286e1
rpalloni <rpdev20@gmail.com>
2021-11-16 21:02:43 +0100

    fix basic

--- tensorflow/tf_basic.py
+++ tensorflow/tf_basic.py
@@ -1,20 +1,40 @@
 '''
-a tensor is a vector/matrix of n dimension that represents all type of data
+https://www.tensorflow.org/guide/tensor
+A tensor is a generalization of vectors and matrices to potentially higher dimensions.
+TF represents tensors as n-dimensional arrays of base datetypes.
+Each tensor has three attributes: datatype (float32, int32, string, etc), shape (axes size) and rank (number of dim)
 
-    | 1 |       | 2  1 |        | | 1 2 | | 5 6 | |
-    | 2 |       | 4  3 |        | | 3 4 | | 8 4 | |
-    | 3 |       | 6  4 |        | | 3 1 | | 2 2 | |
-    vector       matrix               tensor
+                | 1 |       | 2  1 |        | | 1 2 | | 5 6 | |
+    | 1 |       | 2 |       | 4  3 |        | | 3 4 | | 8 4 | |
+                | 3 |       | 6  4 |        | | 3 1 | | 2 2 | |
+    scalar      vector       matrix               tensor
+    rank 0      rank 1       rank 2               rank n
 '''
 import tensorflow as tf
 print(tf.__version__)
 
-# constant tensor
+string = tf.Variable('hello world', tf.string)
+print(tf.rank(string)) # rank 0 tensor => scalar
+string.shape # empty
+integer = tf.Variable(123, tf.int16)
+float = tf.Variable(3.45, tf.float64)
+
+s = tf.Variable(['hello world'], tf.string)
+print(tf.rank(s)) # rank 1 tensor => vector
+s.shape
+
+# constant tensor (immutable)
 C = tf.constant([[4, 3], [6, 1]])
+C
 
-# variable tensor
+# variable tensor (mutable)
 V = tf.Variable([[3, 1], [5, 2]])
+print(tf.rank(V)) # rank 2 tensor => matrix
 
+T = tf.Variable([[[3, 1, 4], [5, 2, 7]], [[4, 8, 2], [6, 3, 0]]])
+print(tf.rank(T))
+T.shape
+T
 
 # concat
 concat_row = tf.concat(values=[C, V], axis=0)
@@ -22,10 +42,21 @@ concat_col = tf.concat(values=[C, V], axis=1)
 
 tf.print(concat_col)
 
+# reshape
+R = tf.reshape(T, shape=[3, 2, 2]) # outer to inner sizes of lists
+R
+
+W = tf.reshape(T, shape=[4, -1]) # -1: infer the shape given a list size
+W
+
 # dummy tensor
 Z = tf.zeros(shape=[3, 4], dtype=tf.int32)
-O = tf.ones(shape=[3, 4], dtype=tf.int32)
 Z
+O = tf.ones(shape=[5, 5, 5], dtype=tf.int32)
+O
+
+H = tf.reshape(O, shape=[125]) # flatten values
+H
 
 # random
 R = tf.random.uniform(shape=[3, 4], dtype=tf.float32)
