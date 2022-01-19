3128817c6e4e3dc94f388849c81ebe5f9a8cd564
Roberto Palloni <rpdev20@gmail.com>
2022-01-19 22:26:13 +0100

    lr

--- tensorflow/tf_lr.py
+++ tensorflow/tf_lr.py
@@ -25,8 +25,26 @@ NUMERIC_COLUMNS = ['age', 'fare']
 
 feature_columns = []
 for feature_name in CATEGORICAL_COLUMNS:
-  vocabulary = dftrain[feature_name].unique()
+  vocabulary = dftrain[feature_name].unique() # get a list of all unique values for each col
   feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
 
 for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+def input_function(df, y, epochs=1000, batch_size=3):
+  return tf.data.Dataset.from_tensor_slices((dict(df), y)).repeat(epochs).batch(batch_size)
+
+# function to provide input data for training as minibatches
+# x=lambda:print('hello')
+# x
+def train_input_fn(): return input_function(dftrain, y_train)
+def test_input_fn(): return input_function(dftest, y_test, epochs=1, batch_size=4)
+
+
+le = tf.estimator.LinearClassifier(feature_columns=feature_columns) # estimator
+le.train(train_input_fn)
+result = le.evaluate(test_input_fn)
+print(result['accuracy'])
+
+y_pred = le.predict(test_input_fn)
+list(y_pred) # a dict for each prediction
