f9d06f549cf3009cc6a69543061aa085c12dddf9
rpalloni <rpdev20@gmail.com>
2022-02-01 21:30:43 +0100

    pca

---
+++ statistics/pca.py
@@ -0,0 +1,32 @@
+'''
+Principal Component Analysis is a mathematical technique used for dimensionality reduction.
+Its goal is to reduce the number of features while keeping most of the original information.
+Singular Value Decomposition
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# two vars and ten samples
+X = np.array([(10, 21), (11, 23), (8, 25), (13, 20), (9, 19), (5, 9), (2, 7), (1, 5), (3, 6), (4, 8)])
+X
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.show()
+
+# average of vars
+mu1, mu2 = np.mean(X, axis=0)
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.title('vars mean on axes')
+plt.plot(mu1, 0, 'rs')
+plt.plot(0, mu2, 'gs')
+plt.axis([0, 15, 0, 30])
+for i in range(len(X)):
+    plt.arrow(X[i, 0], X[i, 1], -1, 0, width=0.01, length_includes_head=True, head_width=0.5, head_length=0.2, fill=False, color='green')
+    plt.arrow(X[i, 0], X[i, 1], 0, -3, width=0.01, length_includes_head=True, head_width=0.2, head_length=0.5, fill=False, color='red')
+plt.annotate('X', xy=(mu1, mu2), weight='bold') # center of data
+plt.show()
+
+
+# shift center of data to origin (scale)
