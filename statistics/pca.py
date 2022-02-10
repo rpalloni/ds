1443ae801121fc7e98dc2e1153889fdead55b26e
rpalloni <rpdev20@gmail.com>
2022-02-10 21:37:08 +0100

    pca

--- statistics/pca.py
+++ statistics/pca.py
@@ -6,10 +6,10 @@ Singular Value Decomposition
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.linalg import eig
 
 # two vars and ten samples
-X = np.array([(10, 21), (11, 23), (8, 25), (13, 20), (9, 19), (5, 9), (2, 7), (1, 5), (3, 6), (4, 8)], dtype=float)
-X
+X = np.array([(8, 14), (11, 24), (10, 18), (13, 20), (9, 19), (5, 9), (2, 7), (1, 5), (3, 3), (4, 8)], dtype=float)
 
 plt.scatter(X[:, 0], X[:, 1])
 plt.show()
@@ -30,18 +30,15 @@ plt.show()
 
 
 # shift center of data to origin (scale) and add PCA line
-X[:, 0] = X[:, 0] - mu1
-X[:, 1] = X[:, 1] - mu2
+X1 = (X[:, 0] - mu1).reshape(10, 1)
+X2 = (X[:, 1] - mu2).reshape(10, 1)
 
-mu1, mu2 = np.mean(X, axis=0)
-d = 0.5
+inv = np.linalg.inv(np.dot(X1.T, X1)) # (X'X)^-1
+xy = np.dot(X1.T, X2) # X'y
+m = np.dot(inv, xy) # b = (X'X)^-1 * X'y
 
-pcax = np.linspace(min(X[:, 0]), max(X[:, 0]), len(X))
-pcay = np.linspace(min(X[:, 1]), max(X[:, 1]), len(X))
-
-plt.scatter(X[:, 0], X[:, 1])
+plt.scatter(X1, X2)
+plt.plot(X1, X1*m, '-', color='g') # regression line
 plt.title('X in origin')
-plt.axis([min(X[:, 0])-d, max(X[:, 0])+d, min(X[:, 1])-d, max(X[:, 1])+d])
-plt.annotate('X', xy=(mu1, mu2), weight='bold') # center of data
-plt.plot(pcax, pcay)
+plt.annotate('X', xy=(np.mean(X1), np.mean(X2)), weight='bold') # center of data
 plt.show()
