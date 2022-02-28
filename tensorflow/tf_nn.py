976a5abaefb7d60264970f8385d43e1fdda01fba
rpalloni <rpdev20@gmail.com>
2022-02-28 21:36:43 +0100

    nn images

--- tensorflow/tf_nn.py
+++ tensorflow/tf_nn.py
@@ -8,19 +8,20 @@ from tensorflow import keras
 
 (train_images, train_labels), (test_images, test_labels) = keras.datasets.fashion_mnist.load_data()
 
-train_images.shape # 60k grayscale (1 color dimension) images with 28x28 pixels
-
-train_images[0]
-
+train_images.shape # 60k grayscale images: each image is a 28x28 grid of pixels with a value
+train_images[0] # each image is a row with 784 columns (28 arrays of 28 elements)
+sum(len(train_images[i]) for i in train_images[0])
 train_images[0, 23, 23] # pixel value: 0 (black) - 255 (white)
 
-train_labels.shape
 
+train_labels.shape # 60k clothing articles codes
 train_labels[:10] # clothing articles: t-shirt, trouser, pullover, dress, coat, sandal, shirt, sneaker, bag, boot
 
-class_names = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'boot']
 
 plt.figure()
-plt.imshow(train_images[1], cmap='Greys')
+plt.imshow(train_images[0], cmap='Greys')
 plt.colorbar()
 plt.show()
+
+
+class_names = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'boot']
