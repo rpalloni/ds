e587495940c125f2a8501ff1a68776b7bed00b12
rpalloni <rpdev20@gmail.com>
2022-03-03 21:47:28 +0100

    cnn

--- tensorflow/tf_keras_cnn_multi.py
+++ tensorflow/tf_keras_cnn_multi.py
@@ -1,9 +1,8 @@
-import os
 import PIL
 import pathlib
 import PIL.Image
 import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 '''
@@ -17,9 +16,7 @@ flower_photos
 '''
 
 dataset_url = 'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz'
-data_dir = tf.keras.utils.get_file(origin=dataset_url,
-                                   fname='flower_photos',
-                                   untar=True)
+data_dir = tf.keras.utils.get_file(origin=dataset_url, fname='flower_photos', untar=True)
 data_dir = pathlib.Path(data_dir)
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
@@ -42,7 +39,6 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
-type(train_ds)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
@@ -52,26 +48,41 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+# no images overlap
+set(train_ds.file_paths).intersection(set(val_ds.file_paths))
+
 class_names = train_ds.class_names
 class_names # 0/1/2/3/4
 
-# each batch is a tensor of shape (32, 180, 180, 3): 32 images of height 180,
+
+# ~92 tensors (2936/32)
+len(list(train_ds.as_numpy_iterator()))
+for images, labels in train_ds:
+    print(images.shape, labels.shape)
+
+# each tensor (batch) has shape (32, 180, 180, 3): 32 images of height 180,
 # width 180 and 3 rgb data, and a tensor of shape (32,): 32 labels
-len(list(train_ds.as_numpy_iterator())) # 92 tensors (2936/32)
+list(train_ds.as_numpy_iterator())[0]
 
-for element in train_ds.as_numpy_iterator():
-    print(element)
+# 32 labels
+images_first_tensor__labels = list(train_ds.as_numpy_iterator())[0][1]
+pd.DataFrame(images_first_tensor__labels, columns=['labels'])
 
 
-len(list(train_ds.as_numpy_iterator())[0][0])   # 32 images-labels per tensor
-list(train_ds.as_numpy_iterator())[0][0]
-list(train_ds.as_numpy_iterator())[0][1]
+images_first_tensor__features = list(train_ds.as_numpy_iterator())[0][0]
+len(images_first_tensor__features) # 32 images-labels per tensor
+# 1 image - 32400 pixels - 97200 rgb data
+pixels = [item for items in images_first_tensor__features[0] for item in items] # flatten nested list
+rgbdata = [item for items in pixels for item in items]
+pd.DataFrame(rgbdata)
 
+
+# each tensor is a 32x97200 dataframe (i.e. 32x180x180x3)
 # last tensor first image features (pixels rgb)
-image1data = list(train_ds.as_numpy_iterator())[91][0][0] # [tensor][features/labels][image]
+image1data = list(train_ds.as_numpy_iterator())[91][0][0] # [tensor][features][image]
 len(image1data)     # 180 rows
 len(image1data[0])  # 180 cols
-image1data[0, 0]    # rgb data in a pixel (out of 32400)
+image1data[0, 0]    # 3 rgb data in a pixel => 97200 data
 image1data[179, 179]
 tf.keras.preprocessing.image.array_to_img(image1data)
 
